32da59ab844aa9789d6f80a7a69e1766d26ad2e4
otrexdev <otrexdev@gmail.com>
2025-04-27 23:13:46 +0200

    new versions

--- JS/Versions.js
+++ JS/Versions.js
@@ -1,5 +1,5 @@
 const preclassic = []
-const classic = ["c0.0.11a.json", "c0.0.12a-dev.json", "c0.0.12a-dev2.json", "c0.0.12a-dev3.json"]
+const classic = ["c0.0.11a.json", "c0.0.12a-dev.json", "c0.0.12a-dev2.json", "c0.0.12a-dev3.json", "c0.0.12a.json", "c0.0.12a_01.json"]
 const indev = []
 const infdev = []
 const alpha = []
